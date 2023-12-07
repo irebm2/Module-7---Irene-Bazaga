aa1ee75000782d58b211be42445643ed2d701158
irebm2 <irebazaga@gmail.com>
2023-12-07 22:45:40 +0000

    oki

--- assets/js.js
+++ assets/js.js
@@ -1,2 +1,13 @@
-var CurrentDate = dayjs.now().format('dd/MM/yyyy');
-$('#currentDay').append(CurrentDate);
+// Select the div once when page loads
+var CurrentDay = $('#currentDay');
+
+var CurrentDate = dayjs().format('DD/MM/YYYY');
+
+// Append date once when page loads
+CurrentDay.text(CurrentDate);
+
+// Update time every 1 second
+setInterval(function(){ 
+    var CurrentTime = dayjs().format('HH:mm:ss');
+    CurrentDay.text(CurrentDate + ' ' + CurrentTime);
+}, 1000);
