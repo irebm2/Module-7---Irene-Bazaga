38d937f09f059f325b9e4a749574660f0a21be81
irebm2 <irebazaga@gmail.com>
2023-12-08 15:18:14 +0000

    future and present

--- assets/js.js
+++ assets/js.js
@@ -1,34 +1,49 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
     // Display the current date and time
     let currentDay = $('#currentDay');
-  
+
     function updateTime() {
-      let now = dayjs();
-      currentDay.text(now.format('dddd, D MMM HH:mm:ss'));
-  
-      // Update time every 1 second
-      setTimeout(updateTime, 1000);
+        let now = dayjs();
+        currentDay.text(now.format('dddd, D MMM HH:mm:ss'));
+
+        // Update time every 1 second
+        setTimeout(updateTime, 1000);
     }
-  
+
     updateTime();
-  
+
     // Create timeslots for each hour from 9 AM to 6 PM
     for (let hour = 9; hour <= 18; hour++) {
-      let time = dayjs().hour(hour).minute(0);
-      let timeslot = $('<div>').addClass('timeslot ');
-      let title = $('<div>').addClass('time-title').text(time.format('h A'));
-  
-      const placeholderTxt = `What is the plan for ${time.format('h A')}?`;
-      let textBox = $('<textarea>').addClass('text-box time-block row').attr('placeholder', placeholderTxt);
-  
-      let blockButton = $('<button>').addClass('block-button').text('Block 🔐');
-  
-      blockButton.on('click', () => {
-        timeslot.toggleClass('is-blocked');
-        blockButton.text(timeslot.hasClass('is-blocked') ? 'Unblock 🔓' : 'Block 🔐' );
-      });
-  
-      timeslot.append(title, textBox, blockButton);
-      $('body').append(timeslot);
+        let time = dayjs().hour(hour).minute(0);
+        let timeslot = $('<div>').addClass('timeslot ');
+        let title = $('<div>').addClass('time-title').text(time.format('h A'));
+
+        const placeholderTxt = `What is the plan for ${time.format('h A')}?`;
+        let textBox = $('<textarea>').addClass('text-box time-block row future').attr('placeholder', placeholderTxt);
+
+        let blockButton = $('<button>').addClass('block-button').text('Block 🔐');
+
+        blockButton.on('click', () => {
+            timeslot.toggleClass('is-blocked');
+            blockButton.text(timeslot.hasClass('is-blocked') ? 'Unblock 🔓' : 'Block 🔐');
+            // Add or remove appropriate class based on blocked state
+            if (timeslot.hasClass('is-blocked')) {
+                textBox.removeClass('future').addClass('present');
+            } else {
+                textBox.removeClass('present').addClass('future');
+            }
+            let currentTime = new Date().getTime().format('HH');
+            if (title.text === currentTime.text) {
+                textBox.addClass('past');
+            }
+
+        });
+
+        timeslot.append(title, textBox, blockButton);
+        $('body').append(timeslot);
+
     }
-  });
+});
+
+
