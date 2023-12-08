a38ff85f3dfcbdac242d1e5d3305f3629f965674
irebm2 <irebazaga@gmail.com>
2023-12-08 15:39:45 +0000

    slots ok

--- assets/js.js
+++ assets/js.js
@@ -1,22 +1,18 @@
 $(document).ready(function () {
 
-    // Display the current date and time
     let currentDay = $('#currentDay');
 
     function updateTime() {
         let now = dayjs();
         currentDay.text(now.format('dddd, D MMM HH:mm:ss'));
-
-        // Update time every 1 second
         setTimeout(updateTime, 1000);
     }
 
     updateTime();
 
-    // Create timeslots for each hour from 9 AM to 6 PM
     for (let hour = 9; hour <= 18; hour++) {
         let time = dayjs().hour(hour).minute(0);
-        let timeslot = $('<div>').addClass('timeslot ');
+        let timeslot = $('<div>').addClass('timeslot');
         let title = $('<div>').addClass('time-title').text(time.format('h A'));
 
         const placeholderTxt = `What is the plan for ${time.format('h A')}?`;
@@ -24,26 +20,21 @@ $(document).ready(function () {
 
         let blockButton = $('<button>').addClass('block-button').text('Block 🔐');
 
+        
         blockButton.on('click', () => {
             timeslot.toggleClass('is-blocked');
             blockButton.text(timeslot.hasClass('is-blocked') ? 'Unblock 🔓' : 'Block 🔐');
-            // Add or remove appropriate class based on blocked state
+
             if (timeslot.hasClass('is-blocked')) {
                 textBox.removeClass('future').addClass('present');
             } else {
                 textBox.removeClass('present').addClass('future');
-            }
-            let currentTime = new Date().getTime().format('HH');
-            if (title.text === currentTime.text) {
-                textBox.addClass('past');
-            }
+            }    
 
-        });
+    });
+        
 
         timeslot.append(title, textBox, blockButton);
         $('body').append(timeslot);
-
     }
 });
-
-
